bc38b82d26e0a395453c2d43fb6038c5a7cf32b7
Mark <mark.arias7@gmail.com>
2023-09-07 20:48:44 -0500

    Completion of project.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,20 +2,35 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+// Mark A.
+// 9.6.23
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { _ in
+            sender.alpha = 1.0
+        }
+        sender.alpha = 0.5
+        
+        playSound(soundName: (sender.titleLabel?.text)!)
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
+
 
